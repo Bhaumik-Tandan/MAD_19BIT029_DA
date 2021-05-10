7116338b28409ebffcb8a6e36c983ecf51d44ac5
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2021-05-10 16:58:08 +0530

    Databse Done

--- app/src/main/java/com/example/mad19bit0292/MainActivity.java
+++ app/src/main/java/com/example/mad19bit0292/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mad19bit0292;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -18,35 +19,22 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseDatabase database = FirebaseDatabase.getInstance("https://mad19bit0292-4a203-default-rtdb.asia-southeast1.firebasedatabase.app/");
+    public void red(View v)
+    {
+        Intent i=new Intent(this,Na.class);
+        startActivity(i);
+    }
 
+    public void wr(View v)
+    {
+        Intent i=new Intent(this,Read.class);
+        startActivity(i);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText f=(EditText) findViewById(R.id.n);
-        String name=f.getText().toString();
-        f=(EditText) findViewById(R.id.rn);
-        String rgno=f.getText().toString();
-        DatabaseReference myRef = database.getReference(name);
-        myRef.setValue(rgno);
 
-//        myRef.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                // This method is called once with the initial value and again
-//                // whenever data at this location is updated.
-//                String value = dataSnapshot.getValue(String.class);
-//                Log.d(name, "Value is: " + value);
-//                f.setText(value);
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError error) {
-//                // Failed to read value
-//                Log.w("Bhaumik Tandan", "Failed to read value.", error.toException());
-//            }
-//        });
-
-        }
+    }
 
 }
--- app/src/main/java/com/example/mad19bit0292/Na.java
+++ app/src/main/java/com/example/mad19bit0292/Na.java
@@ -16,27 +16,36 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class Read extends AppCompatActivity {
+public class Na extends AppCompatActivity {
     FirebaseDatabase database = FirebaseDatabase.getInstance("https://mad19bit0292-4a203-default-rtdb.asia-southeast1.firebasedatabase.app/");
     public void reg(View v)
     {
         EditText f=(EditText) findViewById(R.id.n);
         String name=f.getText().toString();
-        f=(EditText) findViewById(R.id.rn);
-        String rgno=f.getText().toString();
         DatabaseReference myRef = database.getReference(name);
-        myRef.setValue(rgno);
-        Context context = getApplicationContext();
-        CharSequence text = "Registered";
-        int duration = Toast.LENGTH_SHORT;
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
 
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(name, "Value is: " + value);
+                TextView f=(TextView) findViewById(R.id.o);
+                f.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(name, "Failed to read value.", error.toException());
+            }
+        });
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_read);
+        setContentView(R.layout.activity_na);
 
     }
 
